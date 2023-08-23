f6638f374bcfcc1765fe9d8aaf3a880277dea2f0
Hasna Akbar Ali <akbarali.hasna@gmail.com>
2023-08-23 15:25:40 +0530

    Create index_of_first_occurrence_string.py

---
+++ index_of_first_occurrence_string.py
@@ -0,0 +1,15 @@
+def strStr(self, haystack, needle):
+        """
+        Given two strings needle and haystack, 
+        return the index of the first occurrence of needle in haystack, 
+        or -1 if needle is not part of haystack.
+        
+        Eg: 
+        
+        Input: haystack = "sadbutsad", needle = "sad"
+        Output: 0
+        Explanation: "sad" occurs at index 0 and 6.
+        The first occurrence is at index 0, so we return 0.
+        
+        """
+        return haystack.find(needle)
