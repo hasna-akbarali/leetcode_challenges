41433e9f2a9b75c7406352cf6f32579fa27a8fff
Hasna Akbar Ali <akbarali.hasna@gmail.com>
2023-08-23 15:23:32 +0530

    Update merge_strings_alternately.py

--- merge_strings_alternately.py
+++ merge_strings_alternately.py
@@ -3,6 +3,14 @@ def mergeAlternately(word1, word2):
         You are given two strings word1 and word2. 
         Merge the strings by adding letters in alternating order, starting with word1. 
         If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+        Eg: Input: word1 = "abc", word2 = "pqr"
+            Output: "apbqcr"
+            Explanation: The merged string will be merged as so:
+            word1:  a   b   c
+            word2:    p   q   r
+            merged: a p b q c r
+            
         """
         new_word = ''
         small = len(word1) if len(word1) < len(word2) else len(word2)
