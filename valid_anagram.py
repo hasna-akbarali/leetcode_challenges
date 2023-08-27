fbd8180c88778d3b758774b8873028cb5b80bcdd
Hasna Akbar Ali <akbarali.hasna@gmail.com>
2023-08-27 20:56:46 +0530

    Update valid_anagram.py

--- valid_anagram.py
+++ valid_anagram.py
@@ -3,6 +3,11 @@ def isAnagram(self, s, t):
         Given two strings s and t, return true if t is an anagram of s, and false otherwise.
         An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
         typically using all the original letters exactly once.
+
+        Example 1:
+        
+        Input: s = "anagram", t = "nagaram"
+        Output: true
         
         """
         s = sorted(s)
