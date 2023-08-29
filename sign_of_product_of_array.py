768ae1578cfc6127671d2e84ae0e7cf96f2d241a
Hasna Akbar Ali <akbarali.hasna@gmail.com>
2023-08-29 13:37:37 +0530

    Update sign_of_product_of_array.py

--- sign_of_product_of_array.py
+++ sign_of_product_of_array.py
@@ -1,21 +1,18 @@
 def arraySign(self, nums):
         """
-       There is a function signFunc(x) that returns:
+        There is a function signFunc(x) that returns:
+        1 if x is positive.
+        -1 if x is negative.
+        0 if x is equal to 0.
+        You are given an integer array nums. 
+        Let product be the product of all values in the array nums.
 
-1 if x is positive.
--1 if x is negative.
-0 if x is equal to 0.
-You are given an integer array nums. Let product be the product of all values in the array nums.
+        Example 1:
 
-Return signFunc(product).
-
- 
-
-Example 1:
-
-Input: nums = [-1,-2,-3,-4,3,2,1]
-Output: 1
-Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+        Input: nums = [-1,-2,-3,-4,3,2,1]
+        Output: 1
+        Explanation: The product of all values in the array is 144, and signFunc(144) = 1
+        
         """
 
         prod = 1
